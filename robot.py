b076cb5ecc7fe5b40a6f89b224b490e0d3e0e92a
RohitSasidharan <79235419+RohitSasidharan@users.noreply.github.com>
2021-02-19 02:43:24 +1100

    Final code

--- robot.py
+++ robot.py
@@ -1,25 +1,28 @@
+# Function to find shortest distance
 
-def numMoves(input):
-    x = 0
-    y = 0
-    movesList = input.split(',')
-    for move in movesList:
+class robot(object):
+    def numMoves(input):
+        x = 0
+        y = 0
+        movesList = input.split(',')
+         
+        # Traverse the instruction string
         
-        if (move[0] == 'F'):
-            x = x + int(move[1:])
+        for move in movesList:
+                    
+            if (move[0] == 'F'):
+                x = x + int(move[1:])
  
-        elif(move[0] == 'B'):
-            x = x - int(move[1:])
+            elif(move[0] == 'B'):
+                x = x - int(move[1:])
  
-        elif(move[0] == 'L'):
-            y = y - int(move[1:])
+            elif(move[0] == 'L'):
+                y = y - int(move[1:])
  
-        elif(move[0] == 'R'):
-            y = y + int(move[1:])
-    print(abs((x)+(y)))  
-    
-#Get input from the user
-control=input("Enter the direction:");
-print(control)
-numMoves(control)
+            elif(move[0] == 'R'):
+                y = y + int(move[1:])
+        print("The minimum position required to go to the starting point is",abs((x)+(y)))  
 
+#Get input from the user
+controls=input("Enter the direction:");
+robot.numMoves(controls)    
