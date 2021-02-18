e870688cf93a131caa059e0e1a0af388f2ac8fc5
RohitSasidharan <79235419+RohitSasidharan@users.noreply.github.com>
2021-02-19 02:33:30 +1100

    Code optimisation

--- robot.py
+++ robot.py
@@ -1,31 +1,25 @@
 
-
-# Function to find shortest distance 
-
 def numMoves(input):
-    countUp, countDown = 0, 0
-    countLeft, countRight = 0, 0
+    x = 0
+    y = 0
     movesList = input.split(',')
-    
-    # traverse the instruction string
-    
     for move in movesList:
         
         if (move[0] == 'F'):
-            countUp = countUp + int(move[1:])
+            x = x + int(move[1:])
  
         elif(move[0] == 'B'):
-            countDown = countDown + int(move[1:])
+            x = x - int(move[1:])
  
         elif(move[0] == 'L'):
-            countLeft = countLeft + int(move[1:])
+            y = y - int(move[1:])
  
         elif(move[0] == 'R'):
-            countRight = countRight + int(move[1:])
-    print("Final Position: ", (abs(countRight - countLeft)+abs(countUp - countDown)))  
+            y = y + int(move[1:])
+    print(abs((x)+(y)))  
     
-
 #Get input from the user
 control=input("Enter the direction:");
 print(control)
 numMoves(control)
+
